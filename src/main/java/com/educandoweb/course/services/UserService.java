07dfc5a763b764bb8565860a9cb5125fde2300b9
Anderson Dolce <dolce436@gmail.com>
2021-12-22 15:12:00 -0300

    "User insert"

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -23,4 +23,9 @@ public class UserService {
         Optional<User> obj =  repository.findById(id);
         return obj.get();
     }
+
+    //Inserção no Banco de Dados
+    public User insert(User obj){
+        return repository.save(obj);
+    }
 }
