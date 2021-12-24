df0ae71ca371cb1189d32bdf7eeeb7a124f9f918
Anderson Dolce <dolce436@gmail.com>
2021-12-24 10:46:37 -0300

    "Exceptioon Handling Delete"

---
+++ src/main/java/com/educandoweb/course/services/exceptions/DataBaseException.java
@@ -0,0 +1,10 @@
+package com.educandoweb.course.services.exceptions;
+
+public class DataBaseException extends RuntimeException{
+
+    private static final long serialVersioUID = 1L;
+
+    public DataBaseException(String msg){
+        super(msg);
+    }
+}
